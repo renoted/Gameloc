e1e5743b9ec583189c850c85fa10e7d12a097596
ducreuxr <ducreuxr@gmail.com>
2016-01-08 12:49:46 +0100

    connexion à la base de données

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
-define('DB', 'wf3_session');
+define('DB', 'wf3_gameloc');
 
 $db_options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
