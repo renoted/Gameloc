a43a95bd78f4d8e622165156cac44d5845bad577
ducreuxr <ducreuxr@gmail.com>
2016-01-12 11:23:08 +0100

    nouvelle version de la bdd users

--- admin.php
+++ admin.php
@@ -2,10 +2,10 @@
 
 session_start();
 
-require(__DIR__."/congig/db.php");
+require(__DIR__."/functions.php");
 
 check_logged_in();
-
+print_r($_SESSION);
 if($_SESSION["user"]["role"] !== "admin"){
     echo "Vous vous êtes égaré ?";
     header("HTTP/1.0 403 Forbidden");
